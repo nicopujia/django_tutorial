dfcfcbd68d0b71daffd692635744cf8fdcaf62c4
Nicolás Pujía <nicopujia.dev@gmail.com>
2024-01-20 11:33:28 -0300

    Write views that actually do something

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,19 +1,42 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+
+from .models import Choice, Question
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_questions = Question.objects.order_by("-pub_date")[:5]
+    context = ({"latest_questions": latest_questions})
+    return render(request, "polls/index.html", context)
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
 
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {"question": question})
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"]) # type: ignore
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message": "Please select a choice to vote.",
+            },
+        )
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        results = reverse("polls:results", args=(question.id,)) # type: ignore
+        return HttpResponseRedirect(results)
