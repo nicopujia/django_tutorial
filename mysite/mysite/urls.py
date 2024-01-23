88678d3dcead23146398ad85dd945d87a7fad0d4
Nicolás Pujía <nicopujia.dev@gmail.com>
2024-01-23 13:41:03 -0300

    Install Django Debug Toolbar

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -20,5 +20,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("debug/", include("debug_toolbar.urls")),
     path("polls/", include("polls.urls")),
 ]
